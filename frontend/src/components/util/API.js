caec4011c25418e6beedeed4c9d34a8e05f0fb17
alex90271 <alex.alder1@gmail.com>
2021-07-03 11:04:04 -0600

    kinda working

--- frontend/src/components/util/API.js
+++ frontend/src/components/util/API.js
@@ -1,6 +1,15 @@
 const API = {
     send() {
-        console.log("Print was fired");
+        fetch('127.0.0.1:5000/v01/config/') 
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.blob();
+            })
+            .then(myBlob => {
+                console.log(myBlob);
+            })
     }
 }
 
