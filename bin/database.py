814867b24345fef9c04597cc18a8db0e025a184f
alex90271 <alex.alder1@gmail.com>
2021-07-26 22:05:33 -0600

    added hash compare func, work on database

--- bin/database.py
+++ bin/database.py
@@ -1,4 +1,7 @@
+#from bin.query_db import QueryDB
 import sqlite3
+from query_db import QueryDB
+from sqlite3.dbapi2 import DataError
 
 class DatabaseHandler():
     def __init__(self, date) -> None:
@@ -9,10 +12,15 @@ class DatabaseHandler():
 
     def save(self):
         self.connection.commit()
+
+    def close(self):
         self.connection.close()
 
     def create_schema(self):
+        '''Creates and ensures the schema is correct -- and returns a cursor object'''
+        #could use some cleanup
         cur = self.cursor
+        #create the table for net_changes. if it exsits, try putting test data into it
         try:
             cur.execute('''CREATE TABLE net_changes
                (DATE text, 
@@ -29,8 +37,12 @@ class DatabaseHandler():
         except:
             print('using existing net_changes table')
         finally:
-            cur.execute("INSERT INTO net_changes VALUES (?,?,?,?,?,?,?,?,?,?)", (self.date,'TEST','TEST','*TEST* Added 250 Meals',0,0,0,0,0,250))
-
+            try:
+                cur.execute("INSERT INTO net_changes VALUES (?,?,?,?,?,?,?,?,?,?)", 
+                    (self.date,'TEST','TEST','*TEST* Added 250 Meals',0,0,0,0,0,250))
+            except:
+                raise DataError('Schema is invalid')
+        #table for daily calculations, to implement a sort of caching system
         try:
             cur.execute('''CREATE TABLE daily_calc_cache
                (date text, 
@@ -41,10 +53,31 @@ class DatabaseHandler():
         except:
             print('using existing calc table')
         finally:
-            cur.execute("INSERT INTO daily_calc_cache VALUES (?,?,?,?)", ('20210230','8a4751c078d26d89ab5a4fe8647e4fafe46d4bde06eec834fa5949d522d40b32', 12.075865365,18.997778955))
+            try:
+                cur.execute("INSERT INTO daily_calc_cache VALUES (?,?,?,?)", 
+                    ('20210230','8a4751c078d26d89ab5a4fe8647e4fafe46d4bde06eec834fa5949d522d40b32', 12.075865365,18.997778955))
+            except: 
+                raise DataError('Schema is invalid')
         
         self.save()
+        return cur
+    
+    def write_net_changes(self, changes):
+        pass
+    def write_daily_cache(self, cache):
+        pass
 
 if __name__ == '__main__':
     print('loading database.py')
-    DatabaseHandler('20210419').create_schema()
+    day = '20210419'
+    cur = DatabaseHandler(day).create_schema()
+    data = (
+        '20210231',
+        '8a4751c078d26d89ab5a4fe8647e4fafe46d4bde06eec834fa5949d522d40b32', 
+        12.075865365,
+        15.997778955
+        )
+    cur.executemany("INSERT INTO daily_calc_cache VALUES (?,?,?,?)",
+        (data))
+    print('done')
+
--- bin/file_hasher.py
+++ bin/file_hasher.py
@@ -1,21 +1,42 @@
 import hashlib
+import re
 
-file = ".\ADJTIME.Dbf"
-BLOCK_SIZE = 65536
+class FileHash():
+    def __init__(self):
+        pass
 
-first_hash = '8a4751c078d26d89ab5a4fe8647e4fafe46d4bde06eec834fa5949d522d40b31'
+    def hash(self, file):
+        BLOCK_SIZE = 65536
+        file_hash = hashlib.sha256()
+        with open(file, 'rb') as f: 
+            fb = f.read(BLOCK_SIZE)
+            while len(fb) > 0: 
+                file_hash.update(fb) 
+                fb = f.read(BLOCK_SIZE)
 
-file_hash = hashlib.sha256()
-with open(file, 'rb') as f: 
-    fb = f.read(BLOCK_SIZE)
-    while len(fb) > 0: 
-        file_hash.update(fb) 
-        fb = f.read(BLOCK_SIZE)
+        return file_hash.hexdigest()
 
-updated_hash = file_hash.hexdigest()
+    def compare(self, hash_one, hash_two):
 
-if updated_hash == first_hash:
-    print('same file')
-else:
-    print('The SHA256 Hash has changed')
-    print(updated_hash)
+        try:
+            hash_one = self.hash(hash_one)
+        except:
+            print(f'{hash_one} is not a file - assuming it is a hash')
+        try:
+            hash_two = self.hash(hash_two)
+        except:
+            print(f'{hash_two} is not a file - assuming it is a hash')
+
+        print(hash_one, hash_two)
+
+        if hash_one == hash_two:
+            print('Hash Values Match')
+            return True
+        else:
+            print('Hashes do not match')
+            return False
+
+if __name__ == '__main__':
+
+    value = FileHash().compare("8a4751c078d26d89ab5a4fe8647e4fafe46d4bde06eec834fa5949d522d40b31", ".\database\\20210416\ADJTIME.Dbf")
+    print(value)
