356b57dec69db24162f8315caed0b00287bd80a9
ManyPotatoes <62626700+ManyPotatoes@users.noreply.github.com>
2021-09-03 18:53:54 -0600

    Add Employee and Jobcode Selection, ConfigArea and App.js refactor for modularity.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
 import './App.css';
 import React, { useState } from 'react';
-import API from './components/util/API.js';
 import ConfigArea from './components/ConfigArea/ConfigArea';
 import DataTable from './components/DataTable/DataTable';
-import { defaultSettingsObject } from './defaultSettingsObject';
 
 function App() {
 
-  const [settings, setSettings] = useState(defaultSettingsObject);
   const [tableEdited, setTableEdited] = useState(false);
   const [editedTableData, setEditedTableData] = useState();
-  const [selectedDayRange, setSelectedDayRange] = useState({
-    from: null,
-    to: null
-  });
 
-  //converts the selectedDayRange(.from or .to) object into yyyymmdd format for use with the API.
-  //1 is subtracted from range.month as the Date constructor uses a month index starting at 0 for January.
-  const formatDate = (range) => {
-    return new Date(range.year, range.month - 1, range.day).toISOString().slice(0, 10).replace(/-/g, "");
-  }
+  const columnsToRound = ["HOURS", "OVERHRS", "SRVTIPS", "TIPOUT", "DECTIPS", "MEALS"];
+  const editableColumns = ["HOURS", "OVERHRS", "SRVTIPS", "TIPOUT", "DECTIPS", "MEALS"];
 
   const editTable = (row, column, newValue) => {
     setTableEdited(true);
@@ -33,40 +23,15 @@ function App() {
     });
   }
 
-  function handleSettingChange(newSetting) {
-    if (newSetting.displayName && newSetting.outputName && newSetting.dataType && (typeof(newSetting.value) !== 'undefined')) {
-      setSettings((prevSettings) => ({
-        ...prevSettings, [newSetting.outputName]: newSetting
-      }));
-    } else {
-      throw Error("Something went wrong. (the newSetting object is not complete)");
-    }
-  }
-
   // const revertBackToOriginal = () => {
   //   setTableEdited(false);
   // }
 
-  const columnsToRound = ["HOURS", "OVERHRS", "SRVTIPS", "TIPOUT", "DECTIPS", "MEALS"];
-  const editableColumns = ["HOURS", "OVERHRS", "SRVTIPS", "TIPOUT", "DECTIPS", "MEALS"];
-
-  function print() {
-    const range = selectedDayRange;
-    API.test(formatDate(range.from), formatDate(range.to))
-      .then(data => {
-        //console.log(data);
-        setEditedTableData(data);
-      })
-  }
-
   return (
     <div className="App">
       <ConfigArea
-        print={print}
-        selectedDayRange={selectedDayRange}
-        setSelectedDayRange={setSelectedDayRange}
-        settings={settings}
-        handleSettingChange={handleSettingChange}
+      editTable={editTable}
+      setEditedTableData={setEditedTableData}
       />
       <DataTable
         tableData={editedTableData}
--- frontend/src/components/ConfigArea/ConfigArea.js
+++ frontend/src/components/ConfigArea/ConfigArea.js
@@ -5,12 +5,19 @@ import { Calendar } from 'react-modern-calendar-datepicker';
 import PrintArea from '../PrintArea/PrintArea.js';
 import Settings from '../Settings/Settings.js';
 import API from '../util/API.js';
+import { defaultSettingsObject } from '../../defaultSettingsObject';
 
 export default function ConfigArea(props) {
 
     const [canPrint, setCanPrint] = useState(false);
     const [jobcodes, setJobcodes] = useState([]);
     const [employees, setEmployees] = useState([]);
+    const [settings, setSettings] = useState(defaultSettingsObject);
+    const [selectedDayRange, setSelectedDayRange] = useState({
+        from: null,
+        to: null
+    });
+    let displayedRange = '';
 
     const toggleJobcode = (id) => {
         setJobcodes((prevState) => {
@@ -55,42 +62,89 @@ export default function ConfigArea(props) {
     }, []);
 
     useEffect(() => {
-        if (props.selectedDayRange.from && props.selectedDayRange.to) {
+        if (selectedDayRange.from && selectedDayRange.to) {
             setCanPrint(true);
+            if (selectedDayRange.from.day === selectedDayRange.to.day) {
+                displayedRange = `Date selected: ${selectedDayRange.from.month}/${selectedDayRange.from.day}/${selectedDayRange.from.year}`
+            } else {
+                displayedRange = `Dates selected: ${selectedDayRange.from.month}/${selectedDayRange.from.day}/${selectedDayRange.from.year} - ${selectedDayRange.to.month}/${selectedDayRange.to.day}/${selectedDayRange.to.year}`;
+            }
         } else {
             setCanPrint(false);
+            displayedRange = 'Select first and last day. If calculating a single day, click it twice.';
         }
-    }, [props.selectedDayRange]);
+    }, [selectedDayRange]);
 
-    let displayedRange = '';
-    if (props.selectedDayRange.from && props.selectedDayRange.to) {
-        if (props.selectedDayRange.from.day === props.selectedDayRange.to.day) {
-            displayedRange = `Date selected: ${props.selectedDayRange.from.month}/${props.selectedDayRange.from.day}/${props.selectedDayRange.from.year}`
-        } else {
-            displayedRange = `Dates selected: ${props.selectedDayRange.from.month}/${props.selectedDayRange.from.day}/${props.selectedDayRange.from.year} - ${props.selectedDayRange.to.month}/${props.selectedDayRange.to.day}/${props.selectedDayRange.to.year}`;
-        }
+
+
+
+
+
+
+
+
+
+  
+
+  //converts the selectedDayRange(.from or .to) object into yyyymmdd format for use with the API.
+  //1 is subtracted from range.month as the Date constructor uses a month index starting at 0 for January.
+  const formatDate = (range) => {
+    return new Date(range.year, range.month - 1, range.day).toISOString().slice(0, 10).replace(/-/g, "");
+  }
+
+  const selectedToCSV = (inputArray) => {
+      let output = [];
+      inputArray.forEach((item) => {
+          if (item.SELECTED === true) {
+              output.push(item.ID);
+          }
+      });
+      if (output.length === 0) {
+          return "0";
+      } else {
+          return output.join();
+      }
+  }
+
+  
+
+  function handleSettingChange(newSetting) {
+    if (newSetting.displayName && newSetting.outputName && newSetting.dataType && (typeof(newSetting.value) !== 'undefined')) {
+      setSettings((prevSettings) => ({
+        ...prevSettings, [newSetting.outputName]: newSetting
+      }));
     } else {
-        displayedRange = 'Select first and last day. If calculating a single day, click it twice.';
+      throw Error("Something went wrong. (the newSetting object is not complete)");
     }
+  }
+
+  function print() {
+    const range = selectedDayRange;
+    API.test(formatDate(range.from), formatDate(range.to), selectedToCSV(jobcodes), selectedToCSV(employees))
+      .then(data => {
+        //console.log(data);
+        props.setEditedTableData(data);
+      })
+  }
 
     return (
         <div className="ConfigArea">
             <div className='CustomCalendar'>
                 <Calendar
-                    value={props.selectedDayRange}
-                    onChange={props.setSelectedDayRange}
+                    value={selectedDayRange}
+                    onChange={setSelectedDayRange}
                     inputPlaceholder='Select a day'
                     shouldHighlightWeekends
                 />
             </div>
             <PrintArea
                 canPrint={canPrint}
-                print={props.print}
+                print={print}
                 displayedRange={displayedRange}
             />
             <Settings
-                settings={props.settings}
-                handleSettingChange={props.handleSettingChange}
+                settings={settings}
+                handleSettingChange={handleSettingChange}
                 jobcodes={jobcodes}
                 toggleJobcode={toggleJobcode}
                 employees={employees}
