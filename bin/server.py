ef50ee46ad457e035e2052983fce610cf5640531
alex90271 <alex.alder1@gmail.com>
2022-04-24 22:24:18 -0600

    add css to html template

--- bin/server.py
+++ bin/server.py
@@ -45,7 +45,7 @@ def print_rpt(html, day_one, day_two, rpt_type, select_jobs, select_emps, opt_pr
         result.reset_index(drop=True, inplace=True)
         result = result.fillna(0) #turn any NaN data to Zero for json compatability
         if html:
-            return render_template('render.html',  tables=[result.to_html(table_id="table")], titles=result.columns.values) #result.to_html(header="true", table_id="table")
+            return render_template('render.html',  tables=[result.to_html(table_id="table", classes="table is-bordered")], titles=result.columns.values) #result.to_html(header="true", table_id="table")
             
         else:
             return result.to_dict(orient='index')
