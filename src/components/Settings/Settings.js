21c141d292fe8a9e09410196da641fbca428481a
alex90271 <alex.alder1@gmail.com>
2022-07-15 09:51:02 -0600

    temporarily disable settings button

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -9,6 +9,30 @@ import DrownMenu from '../DrownMenu/DrownMenu';
 
 export default function Settings(props) {
 
+    const settingsButton = () => {
+        const settings = false //set to true when settings are enabled
+        if (settings) {
+            return <Modal 
+                openButtonLabel={'Settings'}
+                className={'settings-modal'}
+                headerText={'Settings'}
+                innerHTML={
+                    <div style={{height: "100%"}}>
+                    <SettingsList 
+                        settings={props.settings}
+                        handleSettingChange={props.handleSettingChange}
+                    />
+                    <EditedSettingsBox
+                        save={props.saveSettings}
+                        revert={props.revertSettings}
+                        display={props.settingsChanged}
+                    />
+                    </div>
+                }
+            />
+        }
+    }
+
     const getJobcodeName = (object) => {
         return object.SHORTNAME;
     }
@@ -40,24 +64,7 @@ export default function Settings(props) {
 
     return (
         <div className='Settings'>
-            <Modal 
-                openButtonLabel={'Settings'}
-                className={'settings-modal'}
-                headerText={'Settings'}
-                innerHTML={
-                    <div style={{height: "100%"}}>
-                    <SettingsList 
-                        settings={props.settings}
-                        handleSettingChange={props.handleSettingChange}
-                    />
-                    <EditedSettingsBox
-                        save={props.saveSettings}
-                        revert={props.revertSettings}
-                        display={props.settingsChanged}
-                    />
-                    </div>
-                }
-            />
+            {settingsButton}
             <div className='Reports'>
                 <DrownMenu
                     reports={props.reports}
