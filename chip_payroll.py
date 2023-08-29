f5bf80d0fb4f9f719543848b5042535fe1e37767
alex90271 <alex.alder1@gmail.com>
2023-08-28 22:58:27 -0400

    remove the broken "loading" page

--- chip_payroll.py
+++ chip_payroll.py
@@ -3,29 +3,8 @@ from tkinter import ttk
 from gui import MainGui
 
 if __name__ == "__main__":
-    # Create a window
-    window = tk.Tk()
-    window.geometry("400x200")
     icon = "assets\pyroll_ico.ico"
     title = "Payroll and Tipshare report tool"
-    window.iconbitmap(icon)
-    window.wm_title(title)
-    
-    please_wait_message = tk.Label(window, text="Loading.. This will take a moment", font=("Arial", 10))
-    please_wait_message.pack(padx=5)
-    pb = ttk.Progressbar(
-        window,
-        orient='horizontal',
-        mode='indeterminate',
-        length=160
-    )
-    pb.pack(padx=5)
-    pb.start()
+    MainGui(icon=icon, title=title).main_window()
 
-    #launch the main GUI
-    gui = MainGui(icon=icon, title=title)
-    gui.main_window()
-    gui.kill_launch_window(window)
-
-    window.mainloop()
 
